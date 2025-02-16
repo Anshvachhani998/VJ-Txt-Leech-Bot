9410282876f20e1e1464f58f43da8f2ae3023ed6
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-02-16 18:47:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Tech VJ'
 
+@app.route('/play')
+def play_video():
+    video_url = request.args.get('url')
+    if not video_url:
+        return "Video URL missing!", 400
+    return render_template('video.html', video_url=video_url)
 
 if __name__ == "__main__":
     app.run()
